c5404a2ff2b0e4d5cb6e8cf71511a19f89a140a2
Luqman Fanani <luqman.fanani@gmail.com>
2021-08-27 21:46:53 +0800

    call templates html from py file

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
 
+from markupsafe import escape
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-@app.route("/<luqman>")
-def hello(luqman):
-    return f"Hello,{escape(luqman)}!"
+# @app.route('/login')
+# def test():
+#     return render_template('login.html')
+# if __name__ == '__main__':
+#     app.run()    
 
-@app.route('/testing')
-def testing():
-    return 'saya, perintah testing'
+
+@app.route('/login')
+def hello(name=None):
+    return render_template('login.html', name=name)
