64084818a39741ff81d23bfb11bafa936d60b79a
Luqman Fanani <luqman.fanani@gmail.com>
2021-09-02 09:41:19 +0800

    read value from attr form

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect, url_for
 
 from markupsafe import escape
 app = Flask(__name__)
@@ -11,6 +11,24 @@ app = Flask(__name__)
 #     app.run()    
 
 
-@app.route('/login/<loginame>')
-def hello(loginame):
-    return render_template('login.html',loginame=loginame)
+# @app.route('/login/<loginame>')
+# def hello(loginame):
+#     return render_template('login.html',loginame=loginame)
+
+
+# @app.route('/login/', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         return ' testing panggil halaman post' + request.form['username']
+#     else:  
+#         return render_template('login.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        return ' testing panggil halaman post  ' + request.form['username']+ '\n' + request.form['password']
+
+    return render_template('login.html', error=error)
