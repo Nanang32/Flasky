2c40c175e9b34261d9951ac32dc6d28bfe30ec94
Luqman Fanani <luqman.fanani@gmail.com>
2021-09-22 12:26:35 +0800

    succes db insert manual

--- hello.py
+++ hello.py
@@ -40,3 +40,23 @@ def login():
 def getCookie():
     email = request.cookies.get('email_user')
     return  'email yang tersimpan di Cookie adalah' + email
+
+
+@app.route('/handle-data', methods=['POST'])
+def sendb():
+    db = sqlite3.connect('phonebook.sqlite')
+    cursor = db.cursor()
+
+    if request.method == 'POST':
+        nmphone = request.form['namephone']
+        phnumber = request.form['phonenumber']
+
+        cursor.execute('INSERT INTO pbook(namephone,phonenumber) VALUES (?,?)', (nmphone, phnumber))
+        db.commit()
+    # cursor.execute('INSERT INTO pbook(namephone,phonenumber) VALUES ("kot","443")')
+    # db.commit()
+
+    # cursor.execute('SELECT * FROM pbook')
+    # data = cursor.fetchall()
+
+    # return  str(data)
